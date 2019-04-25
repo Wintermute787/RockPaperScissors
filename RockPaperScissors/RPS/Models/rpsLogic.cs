0ae7ac746ccc934aea094b7c027dc7b6731a8339
Joe Barnes and Michael Larragueta <michael@>
2019-04-25 14:07:07 -0700

    Add everything

---
+++ RockPaperScissors/RPS/Models/rpsLogic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace RockPaperScissors
+                           
+{
+    public class rpsLogic
+    {
+        private string _Choice;
+        private string _CPU;
+        Dictionary<int, string> gamerDic = new Dictionary< int, string>(){{1, "Rock"}, {2, "Paper"}, {3, "Scissor"}};
+
+        public rpsLogic(string word)
+        {
+            _Choice = word; 
+        }
+
+        public string GetChoice()
+        {
+            return _Choice;
+        }
+
+        public string cpuWord()
+        {
+            Random random = new Random();
+            _CPU = gamerDic[random.Next(1, 4)];
+            return _CPU;
+        }
+        public string GetCPU()
+        {
+            return _CPU;
+        }
+        public bool CheckWin(string user, string cpu)
+        {
+            if ((user == "rock" && cpu == "scissors") || (user == "scissors" && cpu == "paper") || (user == "paper" && cpu == "rock"))
+            {
+                return true;
+            }
+            else if (user == "rock" && cpu == "paper" || (user == "paper" && cpu == "scissors") || (user == "scissors" && cpu == "rock"))
+            {
+                return false;
+            }
+            else if (user == cpu)
+            {
+                return false;
+            }
+            else return false;
+        }
+    }
+}
---
+++ RockPaperScissors/RPS/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RockPaperScissors
+{
+    class MainClass
+    {
+        public static void Main()
+        {
+            bool CheckToSeeIfTheUserInputRockPaperOrScissorsORQuitButOnlySetToTrueIfTheUserEntersQuit = false;
+
+            while (!CheckToSeeIfTheUserInputRockPaperOrScissorsORQuitButOnlySetToTrueIfTheUserEntersQuit)
+            {
+                Console.WriteLine("Enter Rock/Paper/Scissors/QUIT");
+
+                string ThisHoldsTheUserInputOfRockPaperScissorsOrQuitAndMakesAllOfTheLettersLowerCaseSoThatWeKeepOurCodeSimpleAndDontMakeThingsMessedUpYouKnowWhatIMean = Console.ReadLine().ToLower();
+                CheckToSeeIfTheUserInputRockPaperOrScissorsORQuitButOnlySetToTrueIfTheUserEntersQuit = ThisHoldsTheUserInputOfRockPaperScissorsOrQuitAndMakesAllOfTheLettersLowerCaseSoThatWeKeepOurCodeSimpleAndDontMakeThingsMessedUpYouKnowWhatIMean == "quit";
+                rpsLogic epicodusRpsVariableNameForObjectInRockPaperScissors = new rpsLogic(ThisHoldsTheUserInputOfRockPaperScissorsOrQuitAndMakesAllOfTheLettersLowerCaseSoThatWeKeepOurCodeSimpleAndDontMakeThingsMessedUpYouKnowWhatIMean);
+                bool TrueOrFalseDependindOnifTheUserInputForRockPaPerScissorsBeatsTheCpuRandomChoiceOfRockPaperOrScissors = epicodusRpsVariableNameForObjectInRockPaperScissors.CheckWin(epicodusRpsVariableNameForObjectInRockPaperScissors.GetChoice(), epicodusRpsVariableNameForObjectInRockPaperScissors.cpuWord().ToLower());
+                if (TrueOrFalseDependindOnifTheUserInputForRockPaPerScissorsBeatsTheCpuRandomChoiceOfRockPaperOrScissors && !CheckToSeeIfTheUserInputRockPaperOrScissorsORQuitButOnlySetToTrueIfTheUserEntersQuit)
+                {
+                    Console.WriteLine("CPU choys: " + epicodusRpsVariableNameForObjectInRockPaperScissors.GetCPU());
+                    Console.WriteLine("YOU WIN");
+                }
+                else
+                {
+                    Console.WriteLine("CPU choys: " + epicodusRpsVariableNameForObjectInRockPaperScissors.GetCPU());
+                    Console.WriteLine("YOU SUCK EVEN THOUGH IT MIGHT BE A TIE. WE DIDNT INCLUDE A CHECK FOR THAT. TOO BAD, LOSER");
+                }
+            }
+        }
+    }
+}
